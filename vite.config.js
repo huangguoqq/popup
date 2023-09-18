f0257798c3ecd980e56f48d5b399a88ca1395331
huangguo <huangguo@tngdigital.com.my>
2023-09-18 10:24:10 +0800

    init

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  build: {
+    lib: {
+      entry: 'src/index.js',
+      name: 'YourPackageName',
+    },
+    rollupOptions: {
+      external: ['vue'], // 如果你的组件依赖了其他的库，可以在这里指定外部依赖
+      output: {
+        globals: {
+          vue: 'Vue', // 如果有外部依赖，这里需要指定全局变量的名称
+        },
+      },
+    },
+  },
+};
